1cf952241ae3390f62e1015876078501c396794a
Alumno <Alumno@LAB1504-15>
2024-03-30 12:22:34 -0500

    Agregar archivos de proyecto.

---
+++ LAB03_TINOCO_DAEA/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LAB03_TINOCO_DAEA
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Data Source=LAB1504-15\\SQLEXPRESS;Initial Catalog=prueba;User Id=admin;Password=123456";
+
+            List<Student> students = new List<Student>();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int StudentId =  reader.GetInt32(0);
+                    string FirstName = reader.GetString(1);
+                    string LastName = reader.GetString(2);
+
+                    students.Add( new Student(StudentId, FirstName, LastName));
+                }
+
+                connection.Close();
+
+                dgStudents.ItemsSource = students;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error:", err.ToString());
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "Data Source=LAB1504-15\\SQLEXPRESS;Initial Catalog=prueba;User Id=admin;Password=123456";
+
+            List<Student> students = new List<Student>();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Students", connection);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                DataTable dataTable = new DataTable();
+
+                adapter.Fill(dataTable);
+
+                connection.Close();
+
+                dgStudents.ItemsSource = dataTable.DefaultView;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            string connectionString = "Data Source=LAB1504-15\\SQLEXPRESS;Initial Catalog=prueba;User Id=admin;Password=123456";
+
+            List<Student> students = new List<Student>();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+
+                connection.Open();
+
+                string name = txtName.Text;
+
+                string commandLike = $"SELECT * FROM students WHERE FirstName LIKE '{name}%'";
+                SqlCommand command = new SqlCommand(commandLike, connection);
+
+                //SqlCommand command = new SqlCommand("SELECT * FROM Students where FirstName="+name, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    int StudentId = reader.GetInt32(0);
+                    string FirstName = reader.GetString(1);
+                    string LastName = reader.GetString(2);
+
+                    students.Add(new Student(StudentId, FirstName, LastName));
+                }
+
+                connection.Close();
+
+                dgStudents.ItemsSource = students;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error:", err.ToString());
+            }
+        }
+    }
+}
---
+++ LAB03_TINOCO_DAEA/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB03_TINOCO_DAEA
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Student (int studentId, string firstName, string lastName)
+        {
+            this.StudentId = studentId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
